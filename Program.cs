ad4a2c4fcce436e1ea42701e8d31c597460cecd5
Tarsila <tarsilabarrosfritz05@gmail.com>
2026-06-07 18:47:06 -0300

    ✨ feat: add initial program  implementation

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿namespace NumberGuessingGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // generates the random number
+            Random rand = new Random();
+            int number = rand.Next(101);
+
+            Console.WriteLine(number);  // for testing
+
+            Console.Write("Inform a number: ");
+            int userGuess = Convert.ToInt32(Console.ReadLine());
+
+            if (userGuess == number)
+            {
+                Console.Write($"You win! The number was {number}");
+            } else if (userGuess > number)
+            {
+                Console.Write("Too high.");
+            } else
+            {
+                Console.Write("Too low.");
+            }
+        }  
+    }
+}

298150d59a5eb4e3afa77ae53836b47d975459ea
Tarsila <tarsilabarrosfritz05@gmail.com>
2026-06-08 21:17:56 -0300

    ✨ feat: add attempt counter and guess limit

--- Program.cs
+++ Program.cs
@@ -5,30 +5,39 @@
         static void Main(string[] args)
         {
             bool keepPlaying = true;
+            int attempts = 0;
+            int attemptsLimit = 5;
+            // int level = 0;
+
             // generates the random number
             Random rand = new Random();
             int number = rand.Next(101);
 
             Console.WriteLine(number);  // for testing
 
-            while (keepPlaying)
+            while (keepPlaying && attempts < attemptsLimit)
             {
                 Console.Write("\nInform a number: ");
                 int userGuess = Convert.ToInt32(Console.ReadLine());
 
                 if (userGuess == number)
                 {
-                    Console.Write($"You win! The number was {number}");
+                    Console.WriteLine($"\nYou win! The number was {number}");
+                    Console.WriteLine($"You guessed the correct number in {attempts} attempts.");
                     keepPlaying = false;
                 } else if (userGuess > number)
                 {
-                    Console.Write("Too high.");
+                    Console.WriteLine("\nToo high.");
+                    attempts++;
                 } else
                 {
-                    Console.Write("Too low.");
+                    Console.WriteLine("\nToo low.");
+                    attempts++;
                 }
             }
-            
+
+            Console.WriteLine($"You reached the limit of attempts to guess the number.\nThe number was {number}");
+            // TODO: add dificulty levels
         }  
     }
 }
